090fac8a731b581a0c115fce1e4d6281364e5f4d
Rik Huijzer <rikhuijzer@pm.me>
2021-01-05 19:25:07 +0100

    Set period to nil

--- projects/ysf/seeds/protocols/kct.rb
+++ projects/ysf/seeds/protocols/kct.rb
@@ -30,9 +30,8 @@ questionnaires.each do |name|
   raise "Cannot find questionnaire: #{name}" unless dagboekvragenlijst_id
   db_measurement = protocol.measurements.where(questionnaire_id: dagboekvragenlijst_id).first
   db_measurement ||= protocol.measurements.build(questionnaire_id: dagboekvragenlijst_id)
-  # One-off questionnaire; not repeated.
-  db_measurement.period = 1.days
-  db_measurement.open_duration = 1.months
+  db_measurement.period = nil # Doesn't repeat. One off.
+  db_measurement.open_duration = 1.years
   db_measurement.open_from_offset = 0.days
   db_measurement.reward_points = default_reward_points
   db_measurement.stop_measurement = false
