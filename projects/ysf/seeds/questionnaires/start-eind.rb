21ffeaa8684e0aad5266fd77cb1e9d1da350b449
Rik Huijzer <rikhuijzer@pm.me>
2021-05-05 15:27:31 +0200

    Fix typo

--- projects/ysf/seeds/questionnaires/start-eind.rb
+++ projects/ysf/seeds/questionnaires/start-eind.rb
@@ -528,7 +528,7 @@ questionnaire.key = 'eindvco'
 
 content = [
   create_fysiek_question(),
-  create_fysiek_question(),
+  create_mentaal_question(),
   create_mentaal_question(),
   create_prestatiedruk_question(),
   create_prestatie_question(),
